d169681bf0cbe6f27f8a537987048e2681845f92
Adel Ansari <adelansari.a@gmailcom>
2023-09-17 23:25:30 +0300

    handle recording start, stop and status

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function HomePage(props) {
   const { setAudioStream, setFile } = props;
+
+  // handle recording
+  const [recordingStatus, setRecordingStatus] = useState("inactive");
+  const [audioChunks, setAudioChunks] = useState([]);
+  const [duration, setDuration] = useState(0);
+
+  const mediaRecorder = useRef(null);
+  const mimeType = "audio/webm";
+
+  async function startRecording() {
+    let tempStream;
+    console.log("Start recording");
+
+    try {
+      const streamData = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false,
+      });
+      tempStream = streamData;
+    } catch (err) {
+      console.log(err.message);
+      return;
+    }
+    setRecordingStatus("recording");
+    const media = new MediaRecorder(tempStream, { type: mimeType });
+    mediaRecorder.current = media;
+
+    mediaRecorder.current.start();
+    let localAudioChunks = [];
+    mediaRecorder.current.ondataavailable = (event) => {
+      if (typeof event.data === "undefined") {
+        return;
+      }
+      if (event.data.size === 0) {
+        return;
+      }
+      localAudioChunks.push(event.data);
+    };
+    setAudioChunks(localAudioChunks);
+  }
+
+  async function stopRecording() {
+    setRecordingStatus("inactive");
+    console.log("Stop recording");
+
+    mediaRecorder.current.stop();
+    mediaRecorder.current.onstop = () => {
+      const audioBlob = new Blob(audioChunks, { type: mimeType });
+      setAudioStream(audioBlob);
+      setAudioChunks([]);
+      setDuration(0);
+    };
+  }
 
   return (
     <main className="flex-1 flex flex-col  p-4 gap-3 sm:gap-4 md:gap-5 justify-center text-center pb-20">
